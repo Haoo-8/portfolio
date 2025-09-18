fac3cc1829968baf141b71abe0bd4439bb68ff7f
Haoo-8 <vonhathao20082002@gmail.com>
2025-09-18 16:29:30 +0700

    feat: Update Projects

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header/Header';
 import Hero from './components/Main/Hero';
 import Skills from './components/Main/Skills';
+import Projects from './components/Main/Projects';
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header/>
       <Hero/>
       <Skills/>
+      <Projects/>
     </>
   )
 }
